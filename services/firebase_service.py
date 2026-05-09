b08de2130d71246f3819cc52eacc3d1e302d10f8
estelitabrito <estelitabrito777@gmail.com>
2026-05-09 15:59:55 -0300

    fix: ajustando as credencias para json

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -2,20 +2,20 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 import json
-import base64
 
 db = None
 
 def inicializar_firebase():
     global db
     if not firebase_admin._apps:
-        cred_b64 = os.getenv("FIREBASE_CREDENTIALS_BASE64")
+        cred_json_str = os.getenv("FIREBASE_CREDENTIALS_JSON")
         
-        if cred_b64:
-            cred_json = json.loads(base64.b64decode(cred_b64).decode("utf-8"))
+        print(f"Tamanho do JSON: {len(cred_json_str) if cred_json_str else 'VAZIO'}")
+        
+        if cred_json_str:
+            cred_json = json.loads(cred_json_str)
             cred = credentials.Certificate(cred_json)
         else:
-            # Fallback para desenvolvimento local
             cred = credentials.Certificate("serviceAccountKey.json")
         
         firebase_admin.initialize_app(cred)
