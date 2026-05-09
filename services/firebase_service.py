af259ac1a656ed8c707def6c4c49bcb5d259b0be
estelitabrito <estelitabrito777@gmail.com>
2026-05-09 15:44:47 -0300

    fix: ajustando credenciais para subir api no render

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,19 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
+import json
+import base64
 
 db = None
 
 def inicializar_firebase():
     global db
     if not firebase_admin._apps:
-        cred = credentials.Certificate(
-            os.getenv("FIREBASE_CREDENTIALS", "serviceAccountKey.json")
-        )
+        cred_b64 = os.getenv("FIREBASE_CREDENTIALS_BASE64")
+        
+        if cred_b64:
+            cred_json = json.loads(base64.b64decode(cred_b64).decode("utf-8"))
+            cred = credentials.Certificate(cred_json)
+        else:
+            # Fallback para desenvolvimento local
+            cred = credentials.Certificate("serviceAccountKey.json")
+        
         firebase_admin.initialize_app(cred)
+    
     db = firestore.client()
     return db
 
